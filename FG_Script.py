e7deecc90b65a63ef2dc492322e189de35246ce7
maxjg5019 <maxjg5019@gmail.com>
2025-07-29 16:33:22 +0800

    feat: adjust widget position & add widget radiobutton

--- FG_Script.py
+++ FG_Script.py
@@ -1,7 +1,30 @@
 import tkinter as tk
 
+def make_Command():
+    text = inputArea.get('1.0', 'end-1c')
+    textArray = text.split('\n')
+    testButton['state'] = 'disabled'
+    print(text)
+    print(textArray)
+
+
 window = tk.Tk()
 window.title('FortiGate 批量指令生成腳本')
-window.geometry('400x500')
-window.resizable(True,True)
+window.geometry('400x450')
+window.resizable(False,False)
+
+radioVar = tk.IntVar()
+radio_Subnet = tk.Radiobutton(text='產出 Subnet',variable=radioVar,value=1,font=('Times New Roman',10))
+radio_FQDN = tk.Radiobutton(text='產出 FQDN',variable=radioVar,value=2,font=('Times New Roman',10))
+radio_Subnet.place(x=50,y=20,anchor='nw')
+radio_FQDN.place(x=350,y=20,anchor='ne')
+radio_Subnet.select()
+
+testButton = tk.Button(text='產出指令',command=make_Command,font=('Times New Roman',14))
+testButton.pack(side='bottom',fill='x',pady=15,padx=15)
+testButton.config(bg='#99ff99')
+
+inputArea = tk.Text(window,width=40,height=20)
+inputArea.place(x=200,y=120,anchor='n')
+
 window.mainloop()
